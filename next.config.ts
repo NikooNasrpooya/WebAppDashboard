6ea9ad0b3f4916f11f2c07fd47fb3f4ce43155d0
Nikoo Nasrpooya <nikoonasrpooya@yahoo.com>
2025-08-12 09:28:07 -0400

    Prepare for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
-
+const REPO = 'WebAppDashboard'
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',                 // needed for GitHub Pages
+  basePath: `/${REPO}`,             // assets & routes served under /REPO
+  assetPrefix: `/${REPO}/`,
+  images: { unoptimized: true },    // for <Image/> on static export
+  trailingSlash: true,              // helps with relative asset paths
 };
 
 export default nextConfig;
