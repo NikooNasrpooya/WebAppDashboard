0dca6498eb1308ecf1847a6a63f264ff7fac944b
Nikoo Nasrpooya <nikoonasrpooya@yahoo.com>
2025-08-12 10:52:24 -0400

    next confige is updated

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
-const REPO = 'WebAppDashboard'
+
 const nextConfig: NextConfig = {
-  output: 'export',                 // needed for GitHub Pages
-  basePath: `/${REPO}`,             // assets & routes served under /REPO
-  assetPrefix: `/${REPO}/`,
-  images: { unoptimized: true },    // for <Image/> on static export
-  trailingSlash: true,              // helps with relative asset paths
+
 };
 
 export default nextConfig;
