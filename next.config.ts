1f92210030ecbd6dd73afee0e4ab4333ca02bbb6
Nikoo Nasrpooya <nikoonasrpooya@yahoo.com>
2025-08-24 13:50:20 -0400

    next config updated

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,8 @@
 // next.config.ts
 import type { NextConfig } from "next";
 
-const isGH = process.env.GH_PAGES === "true";
-const repo = "MovieCatalogue"; // e.g. "task-2-dashboard"
-
 const nextConfig: NextConfig = {
-  // Required for static hosting
-  output: "export",
-  images: { unoptimized: true },
-
-  // Subpath so assets/links work at https://<user>.github.io/<repo>
-  basePath: isGH ? `/${repo}` : "",
-  assetPrefix: isGH ? `/${repo}/` : "",
-
-  // Makes routes like /page/ resolve to /page/index.html on static hosts
-  trailingSlash: true,
+  
 };
 
 export default nextConfig;
